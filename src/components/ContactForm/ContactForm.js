c7087242c0a9a223c1ca344a3c796ed6373d2750
Ha Tran <tranvietha.2809@gmail.com>
2019-02-24 23:55:11 +0200

    Improve name and email validation

--- src/components/ContactForm/ContactForm.js
+++ src/components/ContactForm/ContactForm.js
@@ -6,23 +6,44 @@ class ContactForm extends Component {
         this.state = {
             name: "",
             email: "",
-            mesasge: "",
+            message: "",
         }
     }
 
-    onInputChange(event){
+    onInputChange = event => {
         const target = event.target;
         const className = target.className;
         this.setState({
             [className] : target.value,
-        }, alert(console.log(this.state)))
+        })
+    }
+
+    emailValidation = event =>{
+        const email = event.target.value;
+        const reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!email.match(reg)){
+            alert("Not a valid email");
+            this.setState({
+                'email': ''
+            })
+        }
+        event.preventDefault();
+    }
+
+    submitForm = () => {
+        
     }
     render (){
         return(
             <div className = "contact-form">
-                <input type = "text" className = "name" onChange = {() => this.onInputChange}></input>
-                <input type = "text" className = "email"></input>
-                <input type = "text" className = "message"></input>
+                <input type = "text" className = "name"  name = "name" onChange = {this.onInputChange} value = {this.state.name}></input>
+                <span style = {this.state.name === ''? {visibility: ''}:{visibility: 'hidden'}}>Please enter your name</span>
+                <br/>
+                <input type = "text" className = "email" onChange = {this.onInputChange} value = {this.state.email} onBlur = {this.emailValidation}></input>
+                <span style = {this.state.email === ''? {visibility: ''}:{visibility: 'hidden'}}>Please enter your email</span>
+                <br/>
+                <input type = "text" className = "message" onChange = {this.onInputChange} value = {this.state.message}></input>
+                <input type = "submit" className = "sendMessage" onSubmit = {this.submitForm}/>
             </div>
         )
     }
