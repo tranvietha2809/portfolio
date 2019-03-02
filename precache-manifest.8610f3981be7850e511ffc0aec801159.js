0a24c05f740019a2fdb366fa0d855df84a121805
Ha Tran <tranvietha.2809@gmail.com>
2019-03-02 03:15:34 +0200

    Updates

---
+++ precache-manifest.8610f3981be7850e511ffc0aec801159.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "566e08f779b22d3e0585",
+    "url": "/portfolio/static/css/main.ad12bb10.chunk.css"
+  },
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "41361e6ce1bdde87b558",
+    "url": "/portfolio/static/js/2.41361e6c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "566e08f779b22d3e0585",
+    "url": "/portfolio/static/js/main.566e08f7.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "41361e6ce1bdde87b558",
+    "url": "/portfolio/static/css/2.67b61ca1.chunk.css"
+  },
+  {
+    "revision": "fa447acf2f73dd21a8ec46335e485cdf",
+    "url": "/portfolio/index.html"
+  }
+];
